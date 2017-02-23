1ac408e17a36d6e08e7dd3fec8421b963e6e694d
danbirle <razvan.birle@gmail.com>
2017-02-23 21:09:22 +0100

    Updates

--- index.php
+++ index.php
@@ -14,51 +14,64 @@
 <body>
 <div class="container">
     <div class="sectionTabContainer">
-        <div class="sectionTab" id="tab1">
+        <div class="sectionTab animateTab" id="tab1">
             <a href="#1">
+                <div class="bgCol"></div>
                 <h2>Tab 1</h2>
             </a>
         </div>
         <div class="sectionTab" id="tab2">
             <a href="#2">
+            <div class="bgCol"></div>
                 <h2>Tab 2</h2>
             </a>
         </div>
         <div class="sectionTab" id="tab3">
             <a href="#3">
+            <div class="bgCol"></div>
                 <h2>Tab 3</h2>
             </a>
         </div>
         <div class="sectionTab" id="tab4">
             <a href="#4">
+            <div class="bgCol"></div>
                 <h2>Tab 4</h2>
             </a>
         </div>
     </div>
     <div class="SectionContainer">
-        <div class="section" id="section1">
+        <div class="section animatePanel" id="section1">
+            <div class="bgCol"></div>
+            <div class="content">
             <h3>Tab 1 content</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             <h3>Duis et elit</h3>
             <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+            </div>
         </div>
         <div class="section" id="section2">
+            <div class="bgCol"></div>
+            <div class="content">
             <h3>Tab 2 content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            <h3>Duis et elit</h3>
-            <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+            <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+            </div>
         </div>
         <div class="section" id="section3">
+            <div class="bgCol"></div>
+            <div class="content">
             <h3>Tab 3 content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            <h3>Duis et elit</h3>
-            <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+            <p>Nunc non elementum metus. Sed vestibulum ullamcorper pharetra. Sed sodales elementum nunc, sit amet lacinia mi pretium quis. Sed dapibus nisi convallis, accumsan dolor ac, vestibulum metus. Ut a lacus eleifend, auctor tellus quis, auctor erat. Integer ex neque, ultrices vel sem ac, egestas feugiat eros. Fusce sit amet commodo nisl. Suspendisse pellentesque lacinia tellus sed viverra. Duis euismod, leo non bibendum consectetur, enim massa luctus libero, at vulputate erat nulla nec enim. Quisque nunc sem, finibus ut efficitur ut, consequat vitae risus. Vivamus sit amet cursus turpis.</p>
+            </div>
         </div>
         <div class="section" id="section4">
+            <div class="bgCol"></div>
+            <div class="content">
             <h3>Tab 4 content</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
             <h3>Duis et elit</h3>
             <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+            </div>
         </div>
     </div>
 </div>
--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 $(document).ready(function () {
+    var i=0;
+    var wd;
+    var bgcl = "#dddddd";
+    $('.section .bgCol').each(function () {
+        wd = $('.section').width() / 4 * i;
+        $(this).css('left', wd+'px');
+        i++;
+    });
     $('.sectionTab a').click(function () {
+
+        $('.sectionTab').removeClass('animateTab');
+        $('.bgCol').css('background-color', bgcl);
+        $(this).parent().addClass('animateTab');
+
         var selector = $(this).attr('href');
         selector = selector.substr(1, 1);
-        console.log('section'+selector);
-        $('.section').hide();
-        $('#section' + selector).show();
+
+        $('.section').removeClass('animatePanel');
+        $('#section' + selector).addClass('animatePanel');
+
+        if (bgcl=='#dddddd') {
+            bgcl = '#eeeeee';
+        } else {
+            bgcl = '#dddddd';
+        }
+        $('.SectionContainer').css('background-color', bgcl);
     });
+    
 });
+function cutPx(x) {
+    var lng = x.length;
+    return parseInt(x.substr(0,lng-2));
+}
