b3f0b433c7e6a12cf244438991dd6bb3cd3d76c9
Dan <razvan.birle@gmail.com>
2017-02-24 00:21:51 +0100

    phase1

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
 </head>
 
 <body>
-<div class="container">
+<div class="container_tabs">
+    <h1 style="font-size: 30px;">Tabs</h1>
     <div class="sectionTabContainer">
         <div class="sectionTab animateTab" id="tab1">
             <a href="#1">
@@ -42,37 +43,47 @@
     <div class="SectionContainer">
         <div class="section animatePanel" id="section1">
             <div class="bgCol"></div>
-            <div class="content">
-            <h3>Tab 1 content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            <h3>Duis et elit</h3>
-            <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+            <div class="contentPlace">
+                <div class="content">
+                    <h3>Tab 1 content</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    <h3>Duis et elit</h3>
+                    <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+
+                </div>
             </div>
         </div>
         <div class="section" id="section2">
             <div class="bgCol"></div>
-            <div class="content">
-            <h3>Tab 2 content</h3>
-            <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+            <div class="contentPlace">
+                <div class="content">
+                    <h3>Tab 2 content</h3>
+                    <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+                </div>
             </div>
         </div>
         <div class="section" id="section3">
             <div class="bgCol"></div>
-            <div class="content">
-            <h3>Tab 3 content</h3>
-            <p>Nunc non elementum metus. Sed vestibulum ullamcorper pharetra. Sed sodales elementum nunc, sit amet lacinia mi pretium quis. Sed dapibus nisi convallis, accumsan dolor ac, vestibulum metus. Ut a lacus eleifend, auctor tellus quis, auctor erat. Integer ex neque, ultrices vel sem ac, egestas feugiat eros. Fusce sit amet commodo nisl. Suspendisse pellentesque lacinia tellus sed viverra. Duis euismod, leo non bibendum consectetur, enim massa luctus libero, at vulputate erat nulla nec enim. Quisque nunc sem, finibus ut efficitur ut, consequat vitae risus. Vivamus sit amet cursus turpis.</p>
+            <div class="contentPlace">
+                <div class="content">
+                    <h3>Tab 3 content</h3>
+                    <p>Nunc non elementum metus. Sed vestibulum ullamcorper pharetra. Sed sodales elementum nunc, sit amet lacinia mi pretium quis. Sed dapibus nisi convallis, accumsan dolor ac, vestibulum metus. Ut a lacus eleifend, auctor tellus quis, auctor erat. Integer ex neque, ultrices vel sem ac, egestas feugiat eros. Fusce sit amet commodo nisl. Suspendisse pellentesque lacinia tellus sed viverra. Duis euismod, leo non bibendum consectetur, enim massa luctus libero, at vulputate erat nulla nec enim. Quisque nunc sem, finibus ut efficitur ut, consequat vitae risus. Vivamus sit amet cursus turpis.</p>
+                </div>
             </div>
         </div>
         <div class="section" id="section4">
             <div class="bgCol"></div>
-            <div class="content">
-            <h3>Tab 4 content</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            <h3>Duis et elit</h3>
-            <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+            <div class="contentPlace">
+                <div class="content">
+                    <h3>Tab 4 content</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    <h3>Duis et elit</h3>
+                    <p>Morbi non lacus sed felis luctus ornare a auctor neque. Duis et elit eget purus vehicula aliquet id vitae elit. Aliquam at felis et leo tincidunt interdum eleifend ac augue. </p>
+                </div>
             </div>
         </div>
+        <a href="#" class="buttonForTabs">Next</a>
     </div>
 </div>
 </body>
--- script.js
+++ script.js
@@ -2,31 +2,52 @@ $(document).ready(function () {
     var i=0;
     var wd;
     var bgcl = "#dddddd";
-    $('.section .bgCol').each(function () {
-        wd = $('.section').width() / 4 * i;
+    $('.container_tabs .section .bgCol').each(function () {
+        wd = $('.container_tabs .section').width() / 4 * i;
+        nwd = -wd+20;
         $(this).css('left', wd+'px');
+        $(this).siblings('.contentPlace').css('left', wd+'px');
+        $(this).siblings('.contentPlace').find('.content').css('margin-left', nwd+'px');
         i++;
     });
-    $('.sectionTab a').click(function () {
+    $('.container_tabs .sectionTab a').click(function () {
+        var $this=$(this);
+        changeTabs($this);
+    });
 
-        $('.sectionTab').removeClass('animateTab');
-        $('.bgCol').css('background-color', bgcl);
-        $(this).parent().addClass('animateTab');
+    $('.buttonForTabs').click(function () {
+        var $crt = $('.container_tabs .sectionTab.animateTab a');
+        var $crthref = $crt.attr('href').substr(1,1);
+        var $nxt = Number($crthref) + 1;
+        if ($nxt > 4) { $nxt=4; }
+        $nxt = '#' + $nxt;
+        changeTabs($(".container_tabs .sectionTab a[href='"+$nxt+"']"));
+    });
 
-        var selector = $(this).attr('href');
+
+    function changeTabs(tabItem) {
+        $('.container_tabs .sectionTab').removeClass('animateTab');
+        $('.container_tabs .bgCol').css('background-color', bgcl);
+
+        
+        tabItem.parent().addClass('animateTab');
+        var selector = tabItem.attr('href');
         selector = selector.substr(1, 1);
 
-        $('.section').removeClass('animatePanel');
-        $('#section' + selector).addClass('animatePanel');
+        $('.container_tabs .content').width($('.SectionContainer').width()-40);
+        $('.container_tabs .content').height($('.SectionContainer').height());
+
+        $('.container_tabs .section').removeClass('animatePanel');
+        $('.container_tabs #section' + selector).addClass('animatePanel');
 
         if (bgcl=='#dddddd') {
             bgcl = '#eeeeee';
         } else {
             bgcl = '#dddddd';
         }
-        $('.SectionContainer').css('background-color', bgcl);
-    });
-    
+
+        $('.container_tabs .SectionContainer').css('background-color', bgcl);
+    }
 });
 function cutPx(x) {
     var lng = x.length;
